f72ce9a715af813845ce53eeb9b04a6dddfffab1
lornsharp <156704675+lornsharp@users.noreply.github.com>
2024-01-15 23:00:15 +0400

    Delete Q2/configure_asr_rules.ps1

--- Q2/configure_asr_rules.ps1
+++
@@ -1,7 +0,0 @@
-# Configure Attack Surface Reduction rules
-
-# Define the ASR rule state
-$asrRuleState = "26190899-1602-49e8-8b27-eb1d0a1ce869"
-
-# Configure ASR rule state
-Set-MpPreference -AttackSurfaceReduction_Ids_Global_Actions_ExecutableRules_EnforcementMode $asrRuleState
